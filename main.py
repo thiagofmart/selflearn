ef24bd3d8c97ca7fb39a83a14cc3791c0274afa7
Thiago <thiago.fm2@hotmail.com>
2022-12-12 15:00:43 -0300

    Adjusting Sine function

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ st.markdown("""
 frequency = st.slider("F = Frequency", min_value=0.5, max_value=5.0, value=1.0)
 amplitude = st.slider("A = Amplitude", min_value=0.5, max_value=5.0, value=1.0)
 period = st.slider("P = Period", min_value=np.pi, max_value=5*np.pi, value=2*np.pi)
-hshift = st.slider("Hs = Horizontal Shift", min_value=-3.0, max_value=3.0, value=0.0)
+hshift = st.slider("Hs = Horizontal Shift", min_value=-3.0, max_value=6.0, value=0.0)
 vshift = st.slider("Vs = Vertical Shift", min_value=-4.0, max_value=4.0, value=0.0)
 
 fig = px.line(
     get_sin_df(frequency, amplitude, period, hshift, vshift),
-    title="f(x) = A * sin( F * (P * x + Hs) ) + Vs",
+    title="x = [0, P]+ Hs <=> f(x) = A * sin( F * (x + Hs) ) + Vs",
     range_x=[-5*np.pi, 5*np.pi],
     range_y=[-5, 5]
 )
--- utils.py
+++ utils.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 def get_sin_df(frequency: float|int=1, amplitude: float|int=1, period: float|int=2*np.pi, hshift: float|int=0, vshift: float|int=0):
     f = lambda x: np.sin(x)
-    x_arr = np.arange(0 + hshift, period+0.02 + hshift, 0.1)
-    y_arr = amplitude * f(frequency * x_arr ) + vshift
+    x_arr = np.arange(0, period, 0.1) + hshift
+    y_arr = amplitude * f(frequency * (x_arr)) + vshift
     df = pd.DataFrame(
         data=y_arr,
         index=x_arr,
