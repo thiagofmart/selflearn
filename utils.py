bbdc46fde3c15f4e7aef0d0a51d4d618b519bab6
Thiago Martins <71234183+thiagofmart@users.noreply.github.com>
2022-12-14 17:56:10 -0300

    Remove typing for apply to the deploy version

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from functools import reduce
 
-def get_sin_df(frequency: float|int=1, amplitude: float|int=1, period: float|int=2*np.pi, hshift: float|int=0, vshift: float|int=0):
+def get_sin_df(frequency=1, amplitude=1, period=2*np.pi, hshift=0, vshift=0):
     f = lambda x: np.sin(x)
     x_arr = np.arange(0, period, 0.1) + hshift
     y_arr = amplitude * f(frequency * (x_arr)) + vshift
