968f6bf6e505a1adbaefffb1ec1f6e7a88a19119
Thiago <thiago.fm2@hotmail.com>
2022-12-15 18:55:33 -0300

    Update Asyncio with threads

--- main.py
+++ main.py
@@ -1,32 +1,60 @@
 import numpy as np
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 import streamlit as st
+from streamlit.runtime.scriptrunner import add_script_run_ctx
+import streamlit.components.v1 as components
 from utils import create_image, get_sin_df, generate_motion_df, update_motion_df
 from time import sleep
 import asyncio
+from threading import Thread
 
+
+frame = 64 # frame per second
+max_velocity = 4
+size_of_entities = 10
+plane_dim = (800, 500)
+if "rendered_images" not in st.session_state:
+    st.session_state['rendered_images'] = []
+    st.session_state['df_entities'] = generate_motion_df(size_of_entities, plane_dim, max_velocity)
+    st.session_state['loop'] = True
 
 placeholder = st.empty()
 checkbox = st.checkbox('show entities')
-async def display_images(frame):
-    global df_entities, plane_dim
+
+async def render_images(plane_dim):
     while True:
-        for i in range(0, 1_000):
-            df_entities = await update_motion_df(df_entities, plane_dim)
-            image = await create_image(plane_dim, df_entities)
-            sleep(1/frame)
-            placeholder.image(image)
+        if not st.session_state.loop:
+            break
+        if len(st.session_state.rendered_images) < 10:
+            while (len(st.session_state.rendered_images) < 250):
+                if not st.session_state.loop:
+                    break
+                st.session_state.df_entities = await update_motion_df(st.session_state.df_entities, plane_dim)
+                image = await create_image(plane_dim, st.session_state.df_entities)
+                st.session_state.rendered_images.append(image)
+                print(f"rendering {len(st.session_state.rendered_images)}")
+
+async def display_images(frame):
+    while True:
+        sleep(1/frame)
+        if not st.session_state.loop:
+            break
+        if st.session_state.rendered_images:
+            placeholder.image(st.session_state.rendered_images.pop(0))
+       
+
 
 if checkbox:
-    frame = 10 # frame per second
-    max_velocity = 7.5
-    size_of_entities = 10
-    plane_dim = (800, 500)
-    df_entities = generate_motion_df(size_of_entities, plane_dim, max_velocity)    
     with placeholder.container():
+        st.session_state.loop = True
+        t1 = Thread(target=lambda x: asyncio.run(render_images(x)), args=(plane_dim,))
+        add_script_run_ctx(t1)
+        t1.start()
         asyncio.run(display_images(frame))
+if not checkbox:
+    st.session_state.loop = False
+
 
 
 ####################################################################
@@ -57,30 +85,5 @@ st.latex(r'''
     \sum_{k=0}^{n-1} ar^k =
     a \left(\frac{1-r^{n}}{1-r}\right)
     ''')
-
-"""
-progress_bar = st.progress(0)
-status_text = st.empty()
-chart = st.line_chart(np.random.randn(10, 2))
-
-for i in range(1, 101):
-    # Update progress bar.
-    progress_bar.progress(i)
-
-    new_rows = np.random.randn(10, 2)
-
-    # Update status text.
-    status_text.text(
-        'The latest random number is: %s' % new_rows[-1, 1])
-
-    # Append data to the chart.
-    chart.add_rows(new_rows)
-
-    # Pretend we're doing some computation that takes time.
-    sleep(0.1)
-
-status_text.text('Done!')
-st.balloons()
-"""
 
 
