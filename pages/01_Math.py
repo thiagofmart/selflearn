b6914bfd4f9ed359efb12efc62dfc8e4ccdab6d7
Thiago <thiago.fm2@hotmail.com>
2022-12-23 17:59:56 -0300

    Change in PI analysis post

--- pages/01_Math.py
+++ pages/01_Math.py
@@ -18,23 +18,16 @@ pi_analysis, sinusoidal_function = st.tabs(posts)
 with pi_analysis:
     st.markdown("""
 # π Analysis
-
+---
+# Interpretations
 π (pronounced "pi") is a mathematical constant that represents the 
 ratio of the circumference C of a circle to its diameter d. 
 $$
 π = {C \over a}
 $$
-It is approximately equal to 3.14159, but it is an irrational number, which 
-means that it cannot be expressed exactly as a decimal. The decimal 
-representation of π goes on indefinitely. It is one of the most well-known
-and widely used mathematical constants in the world and is essential in many
-fields, including geometry, trigonometry, and engineering.
-
 The ratio $$C/d$$ is constant, regardless of the circles's size. For example if 
 a circle has twice the diameter of another, it will also have twice the 
 circumference, preserving the ratio $$C/d$$.                    
-
-<img src="assets/giphy.gif" width="100" height="100" />
 
 This definition of π implicitly 
 makes use of flat (Euclidian) geometry; although the notion of a circle  can
@@ -49,9 +42,30 @@ coordinates by the equation $$x^2+y^2=1$$, as the integral:
 $$
 π = {\int_{-1}^1 {dx \over \sqrt{1-x^2}}}
 $$
+One such definition, due to Richard Baltzer and popularized by Edmund Landau, is 
+the following: π is twice the smallest positive number at which the cosine function 
+equals 0. π is also the smallest positive number at which the sine function equals 
+zero, and the difference between consecutive zeroes of the sine function. The cosine 
+can be defined independently of geometry as a power series, or as the solution of a 
+differential equation. In similar spirit π can be defined using properties of the 
+complex exponential, exp $$z$$, of a complex variable $$z$$. Like the cosine, the 
+complex exponential can be defined in one of several ways. The set of complex numbers 
+at which exp $$z$$ is equal to one is then an (imaginary) arythmetic progression of 
+the form:
+$$
+\{..., -2πi, 0, 2πi, 4πi, ...\} = \{2πki | k ∈ ℤ\}
+$$
+and there is an unique positve real number π with this property.
 
+A variation on the same idea, making use of sophisticated mathematical concepts of 
+topology and algebra, is the following theorem: there is a unique (up to automorphism)
+continuous isomorphism from the group R/Z of real numbers under addition modulo integers
+(the circle group), onto the multiplicative group of complex numbers of absolute value 
+one. The number π is then defined as half the magnitude of the deritive of this hmomorphism.
 
----""", unsafe_allow_html=True)
+## Approximations
+
+""", unsafe_allow_html=True)
 
 with sinusoidal_function:
     st.markdown("""
@@ -71,16 +85,5 @@ with sinusoidal_function:
     )
 
     st.plotly_chart(fig)
-
-
-st.text_input("Your name", key="name")
-
-# You can access the value at any point with:
-st.session_state.name
-st.latex(r'''
-    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-    \sum_{k=0}^{n-1} ar^k =
-    a \left(\frac{1-r^{n}}{1-r}\right)
-    ''')
 
 render_footer()
